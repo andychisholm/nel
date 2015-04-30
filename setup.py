d113918466acf1abed864ff4b138b2f5e910c7c3
Andrew Chisholm <andy.chisholm.89@gmail.com>
2015-04-30 10:41:49 +1000

    Bumped version to 0.1.1 and added metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 __pkg_name__ = 'nel'
 
 setup(
     name = __pkg_name__,
     version = __version__,
     description = 'Named entity linker',
+    author='Andrew Chisholm',
     packages = find_packages(),
     license = 'MIT',
     url = 'https://github.com/wikilinks/nel',
@@ -15,6 +16,15 @@ setup(
             'nel = nel.__main__:main'
         ]
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
     install_requires = [
         "Flask",
         "Jinja2",
