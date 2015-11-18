61642c50ab36d50d422478a8d739d548f88ea299
Andrew Chisholm <andy.chisholm.89@gmail.com>
2015-11-18 12:11:08 +1100

    Add candidate and coref modules

---
+++ nel/process/candidates.py
@@ -0,0 +1,103 @@
+import re
+import string
+from itertools import izip
+
+from .process import Process
+from ..model import recognition
+from ..doc import Mention, Chain, Candidate
+from ..model.disambiguation import NameProbability
+
+import logging
+log = logging.getLogger()
+
+class CandidateGenerator(Process):
+    """ Populate candidate sets for chains in a document. """
+    RE_WS = re.compile('\s+')
+    RE_WS_PRE_PUCT = re.compile(u'\s+([^a-zA-Z\d])')
+
+    @classmethod
+    def iter_options(cls):
+        for c in globals().itervalues():
+            if c != cls and isinstance(c, type) and issubclass(c, cls):
+                yield c
+
+    def get_doc_state(self, doc):
+        return None
+
+    def get_candidates(self, doc, name, state):
+        raise NotImplementedError
+
+    def __call__(self, doc):
+        state = self.get_doc_state(doc)
+
+        for chain in doc.chains:
+            forms = set()
+            for m in chain.mentions:
+                forms.update(self.get_normalised_forms(m.text))
+            forms = sorted(forms, key=len, reverse=True)
+
+            candidates = set()
+            for sf in forms:
+                candidates.update(self.get_candidates(doc, chain, sf, state))
+                if candidates:
+                    break
+
+            chain.candidates = [Candidate(c) for c in candidates]
+
+        return doc
+
+    @classmethod
+    def normalise_form(cls, sf):
+        sf = sf.lower()
+        sf = cls.RE_WS_PRE_PUCT.sub(r'\1', sf)
+        sf = cls.RE_WS.sub(' ', sf)
+        return sf
+
+    @classmethod
+    def iter_derived_forms(cls, sf):
+        yield sf
+        yield sf.replace("'s", "")
+        yield ''.join(c for c in sf if not c in string.punctuation)
+
+        comma_parts = sf.split(',')[:-1]
+        for i in xrange(len(comma_parts)):
+            yield ''.join(comma_parts[:i+1])
+        if comma_parts:
+            yield ''.join(comma_parts)
+        
+        colon_idx = sf.find(':')
+        if colon_idx != -1:
+            yield sf[:colon_idx]
+
+        quote_parts = sf.split('"')
+        if len(quote_parts) >= 3:
+            yield ''.join(quote_parts)
+            yield ''.join(quote_parts[:1]+quote_parts[-1:])
+
+    @classmethod
+    def get_normalised_forms(cls, sf):
+        return set(cls.normalise_form(f) for f in cls.iter_derived_forms(sf))
+
+class NameCounts(CandidateGenerator):
+    """ Return candidates for entities with non-zero name posterior. """
+    def __init__(self, name_model_tag, limit):
+        log.info('Preparing name model candidate generator (model=%s, limit=%i)...', name_model_tag, limit)
+        self.nm = NameProbability(name_model_tag)
+        self.limit = limit
+ 
+    @classmethod
+    def add_arguments(cls, p):
+        p.add_argument('name_model_tag', metavar='NAME_MODEL')
+        p.add_argument('--limit', required=False, type=int, default=15, metavar='MAX_CANDIDATES')
+        return p
+
+    def get_doc_state(self, doc):
+        forms = list(set(df for c in doc.chains for m in c.mentions for df in self.get_normalised_forms(m.text)))
+
+        state = {}
+        for sf, eps in self.nm.get_counts_for_names(forms).iteritems():
+            state[sf] = [e for e, c in sorted(eps.iteritems(), key=lambda (k,v):v, reverse=True)][:self.limit]
+        return state
+
+    def get_candidates(self, doc, chain, name, state):
+        return state[name][:self.limit]
---
+++ nel/process/coref.py
@@ -0,0 +1,92 @@
+from collections import defaultdict
+from itertools import chain
+
+from ..doc import Chain
+from .process import Process
+
+import logging
+log = logging.getLogger()
+
+class MentionClusterer(Process):
+    """ Perform in-document coreference clustering over mentions """
+    @classmethod
+    def iter_options(cls):
+        for c in globals().itervalues():
+            if c != cls and isinstance(c, type) and issubclass(c, cls):
+                yield c
+
+    def get_clusters(self, doc, mentions):
+        raise NotImplementedError
+
+    def __call__(self, doc):
+        mentions_to_cluster = []
+
+        # for training documents, we use gold resolutions to cluster non-nil mentions
+        if doc.tag == 'train':
+            mbr = defaultdict(list)
+            for c in doc.chains:
+                for m in c.mentions:
+                    if m.resolution == None:
+                        mentions_to_cluster.append(m)
+                    else:
+                        mbr[m.resolution.id].append(m)
+            doc.chains = [Chain(mentions=ms) for r, ms in mbr.iteritems()]
+        else:
+            mentions_to_cluster = list(chain.from_iterable(c.mentions for c in doc.chains))
+            doc.chains = []
+
+        doc.chains += [Chain(mentions=ms) for ms in self.get_clusters(doc, mentions_to_cluster)]
+        return doc
+
+class SpanOverlap(MentionClusterer):
+    """ Cluster mentions by prefix/suffix overlap and acronym match. """
+    @classmethod
+    def add_arguments(cls, p):
+        return p
+
+    def __init__(self):
+        log.info('Using mention text span-overlap coreference clusterer...')
+
+    def get_clusters(self, doc, mentions):
+        chains = []
+        unchained_mentions = sorted(mentions, key=lambda m:m.begin, reverse=True)
+
+        #log.debug('MENTIONS: ' + ';'.join(m.text for m in unchained_mentions))
+        while unchained_mentions:
+            mention = unchained_mentions.pop(0)
+
+            potential_antecedents = [(m.text, m) for m in unchained_mentions if m.tag == mention.tag]
+            chain = [mention]
+
+            likely_acronym = False
+
+            if mention.text.upper() == mention.text:
+                # check if our mention is an acronym of a previous mention
+                for a, m in potential_antecedents:
+                    if (''.join(p[0] for p in a.split(' ') if p).upper() == mention.text) or \
+                       (''.join(p[0] for p in a.split(' ') if p and p[0].isupper()).upper() == mention.text):
+                        chain.insert(0, m)
+                        unchained_mentions.remove(m)
+                        likely_acronym = True
+                potential_antecedents = [(m.text, m) for m in unchained_mentions]
+
+            last = None
+            longest_mention = mention
+            while last != longest_mention and potential_antecedents:
+                # check if we are a prefix/suffix of a preceding mention
+                n = longest_mention.text.lower()
+                for a, m in potential_antecedents:
+                    na = a.lower()
+                    if (likely_acronym and mention.text == a) or \
+                       (not likely_acronym and (na.startswith(n) or na.endswith(n) or n.startswith(na) or n.endswith(na))):
+                        chain.insert(0, m)
+                        unchained_mentions.remove(m)
+                
+                last = longest_mention
+                longest_mention = sorted(chain, key=lambda m: len(m.text), reverse=True)[0]
+                potential_antecedents = [(m.text, m) for m in unchained_mentions if m.tag == mention.tag]
+
+            chains.append(chain)
+            #log.debug('CHAIN(%i): %s' % (len(potential_antecedents), ';'.join(m.text for m in chain)))
+
+        return chains
