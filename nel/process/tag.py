fdab3b51089fbb98e0f4f97ec0ae8f1469db1b0f
Andrew Chisholm <andy.chisholm.89@gmail.com>
2017-03-19 17:08:56 +1100

    Remove schwa tagger

--- nel/process/tag.py
+++ nel/process/tag.py
@@ -59,11 +59,6 @@ class SpacyTagger(Tagger):
 
 class CRFTagger(Tagger):
     """ Conditional random field sequence tagger """
-    @classmethod
-    def add_arguments(cls, p):
-        p.add_argument('model_tag', metavar='MODEL_TAG')
-        return p
-
     def __init__(self, model_tag):
         log.info('Loading CRF sequence classifier: %s', model_tag)
         self.tagger = recognition.SequenceClassifier(model_tag)
@@ -97,12 +92,6 @@ class CRFTagger(Tagger):
 
 class StanfordTagger(Tagger):
     """ Tag documents via a hosted Stanford NER service """
-    @classmethod
-    def add_arguments(cls, p):
-        p.add_argument('host', metavar='HOSTNAME')
-        p.add_argument('port', type=int, metavar='PORT')
-        return p
-
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -185,115 +174,3 @@ class StanfordTagger(Tagger):
                 yield self.mention_over_tokens(doc, start, i)
 
         log.debug('Tagged doc (%s) with %i tokens in %.2fs', doc.id, len(doc.tokens), time() - start_time)
-
-class SchwaTagger(Tagger):
-    """ Tags named entities using the schwa docrep ner system (Dawborn, 15) """
-    FLT_TAGS = ['date','cardinal','time','percent','ordinal','language','money','quantity']
-    STARTUP_TIMEOUT = 30
-    schema = None
- 
-    @classmethod
-    def add_arguments(cls, p):
-        p.add_argument('ner_package_path', metavar='NER_PACKAGE_PATH')
-        p.add_argument('tagger_path', metavar='TAGGER_PATH')
-        p.add_argument('tokenizer_path', metavar='TOKENIZER_PATH')
-        p.add_argument('ner_model_name', metavar='NER_MODEL_NAME')
-        return p
-
-    def __init__(self, ner_package_path, tagger_path='schwa-ner-tagger', tokenizer_path='schwa-tokenizer', ner_model_name='conll12'):
-        log.info('Initialising libschwa tagger...')
-        self.tagger_path = tagger_path
-        self.tokenizer_path = tokenizer_path
-        self.ner_package_path = ner_package_path
-        self.ner_model_name = ner_model_name
-
-        if self.schema == None:
-            # docrep schema used by the tokeniser and tagger
-            from schwa import dr
-
-            class Token(dr.Ann):
-                raw = dr.Text()
-                norm = dr.Text()
-                pos = dr.Field()
-                lemma = dr.Field()
-                span = dr.Slice()
-                tidx = dr.Field()
-
-            class NamedEntity(dr.Ann):
-                span = dr.Slice(Token)
-                label = dr.Field()
-
-            class SchwaDoc(dr.Doc):
-                doc_id = dr.Field()
-                tokens = dr.Store(Token)
-                named_entities = dr.Store(NamedEntity)
-
-            self.schema = SchwaDoc.schema()
-
-        self.tagger_process = None
-        self.initialise_tagger()
-
-    def initialise_tagger(self):
-        if self.tagger_process != None and self.tagger_process.poll() == None:
-            self.tagger_process.kill()
-
-        # configure the schwa ner tagger
-        self.tagger_process = Popen([
-            self.tagger_path,
-            '--crf1-only', 'true',
-            '--model', os.path.join(self.ner_package_path, self.ner_model_name)
-        ],  cwd = self.ner_package_path, stdout = PIPE, stdin = PIPE, stderr = PIPE)
-
-        t = time()
-        while self.tagger_process.stderr.readline().strip() != 'Tagger Ready':
-            if time() - t > self.STARTUP_TIMEOUT:
-                raise Exception("Tagger startup timeout")
-        log.info('Schwa tagger startup completed in %.1fs', time() - t)
-
-    def text_to_dr(self, text):
-        from schwa import dr
-        tokenizer = Popen([
-            self.tokenizer_path,
-            '-p', 'docrep'
-        ], cwd = self.ner_package_path, stdout = PIPE, stdin = PIPE)
-
-        tok_dr, err = tokenizer.communicate(text)
-        if not tok_dr or err:
-            raise Exception("Schwa tokenizer failed while processing document")
-
-        self.tagger_process.stdin.write(tok_dr)
-        self.tagger_process.stdin.flush()
-
-        try:
-            status = self.tagger_process.stderr.readline().strip()
-            if self.tagger_process.poll() != None or status == '' or status == '0':
-                raise Exception("Schwa tagger failed while processing document")
-
-            try:
-                result_sz = int(status)
-            except ValueError:
-                schwa_error = Exception(status)
-                raise Exception("Schwa tagger error while processing document", schwa_error)
-
-            try:
-                result_bytes = self.tagger_process.stdout.read(result_sz)
-                result = dr.Reader(StringIO(result_bytes), self.schema).read()
-                return result
-            except Exception as e:
-                raise Exception("Failed to deserialise schwa tagger output", e), None, sys.exc_info()[2]
-        except:
-            self.initialise_tagger()
-            raise
-
-    def tag(self, doc):
-        raw = doc.text.encode('utf-8')
-
-        # tagger returns byte offsets for tokens, we need unicode character offsets
-        offset_map = byte_to_char_map(raw)
-        tagged_dr = self.text_to_dr(raw)
-        doc.tokens = [Mention(offset_map[t.span.start]-1, t.raw) for t in tagged_dr.tokens]
-
-        for e in tagged_dr.named_entities:
-            tag = e.label.lower()
-            if tag not in self.FLT_TAGS:
-                yield self.mention_over_tokens(doc, e.span.start, e.span.stop, tag)
